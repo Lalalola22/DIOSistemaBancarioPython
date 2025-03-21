ae6f237e18652ae37db8f53703940e1b391808e9
Lola <105025726+Lalalola22@users.noreply.github.com>
2025-03-20 21:57:32 -0300

    Update sistema bancario.py
    Adição da opção "Transferir" e mensagens de erro.

--- sistema bancario.py
+++ sistema bancario.py
@@ -2,6 +2,7 @@ menu = """
 
 [d] Depositar
 [s] Sacar
+[t] Transferir
 [e] Extrato
 [q] Sair
 
@@ -23,6 +24,7 @@ while True:
         if valor > 0:
             saldo += valor
             extrato += f"Depósito: R$ {valor:.2f}\n"
+            print("Depósito concluído com sucesso!")
 
         else:
             print("Operação falhou! O valor informado é inválido.")
@@ -37,7 +39,7 @@ while True:
         excedeu_saques = numero_saques >= LIMITE_SAQUES
 
         if excedeu_saldo:
-            print("Operação falhou! Você não tem saldo suficiente.")
+            print("Operação falhou! Você não tem saldo suficiente para realizar o saque.")
 
         elif excedeu_limite:
             print("Operação falhou! O valor do saque excede o limite.")
@@ -49,9 +51,50 @@ while True:
             saldo -= valor
             extrato += f"Saque: R$ {valor:.2f}\n"
             numero_saques += 1
+            print("Saque concluído com sucesso!")
 
         else:
             print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "t":
+
+        while True:
+
+            agencia = input("Insira o número da agência para qual deseja transferir um valor (4 dígitos): ")
+            
+            if agencia.isnumeric() and len(agencia) == 4:
+                agencia = int(agencia)  
+                break  
+            else:
+                print("Número de agência inválido! É necessário ter 4 dígitos no total.")
+
+        while True:
+
+            conta = input("Insira o número da conta para qual deseja transferir um valor (5 dígitos): ")
+            
+            if conta.isnumeric() and len(conta) == 5:
+                conta = int(conta)  
+                break
+            else:
+                print("Número de conta inválido! É necessário ter 5 dígitos no total.")
+
+        while True:
+
+            valor = float(input("Informe o valor da transferência: "))
+            
+            excedeu_saldo = valor > saldo
+
+            if excedeu_saldo:
+             print("Operação falhou! Você não tem saldo suficiente para realizar a transferência.")
+
+            elif valor <= 0:
+                print("Operação falhou! É necessário realizar transferências acima de R$ 0.00.")
+
+            else:
+                saldo -= valor
+                extrato += f"Transferir: R$ {valor:.2f}\n"
+                print("Transferência concluída com sucesso!")
+                break
 
     elif opcao == "e":
         print("\n================ EXTRATO ================")
@@ -64,4 +107,3 @@ while True:
 
     else:
         print("Operação inválida, por favor selecione novamente a operação desejada.")
-
